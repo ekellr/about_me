20f2edcebe63a4ff6de94513f813da21181e7827
ekellr <ekellr@hotmail.com>
2017-06-08 00:14:46 -0700

    Updating lab3

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 var userPoints = 0;
 var myName = 'Elaine';
+var movieTimes = 100;
+var numberOfQuestions = 7;
 
 
 alert('Welcome to my guessing game! My name is ' + myName + '!');
@@ -75,6 +77,68 @@ if (answer5 === 'yes' || answer5 === 'y') {
 alert('You have ' + userPoints + ' points.');
 console.log('The user has ' + userPoints + ' points!!!');
 
-//---------------------------------------------------------------
-// This method returns a boolean value indicating whether or not the result
-// is a yes answer.
+var answer6;
+var retryCount = 0;
+
+do{
+  answer6 = prompt('How many time did I watch the movie Aliens?');
+  console.log('The user answer is ' + answer6);
+
+  if(answer6 < movieTimes) {
+    alert('Your guess is too low! Please try again.');
+    console.log('The user answer is too low.');
+  }
+  else {
+    if(answer6 > movieTimes) {
+      alert('Your guess is too high! Please try again.');
+      console.log('The user answer is too high.');
+    }
+    else{
+      alert('You got it right!!!')
+      console.log('The user answer is right.');
+      userPoints++;
+      break;
+    }
+  }
+  retryCount++;
+}
+
+while(retryCount < 5);
+
+var foodsDisike = ['MSG', 'wheat', 'canola oil', 'soy', 'simple carbs'];
+alert('Hi ' + user + '! Let\'s play another game:');
+
+var answer7 = prompt('Can you guess what food I avoid as much as I can?';
+console.log(answer7);
+
+
+
+for (var i = 0; i < 5; i++) {
+  console.log(foodsILike[i]);
+}
+
+foodsILike.push('swiss chard');
+console.log('The user \'s guess was ' + foodsILike);
+
+// pop removes an element from the end of the array
+var veggie = foodsILike.pop();
+console.log('The user \'s guess was ' + foodsILike);
+
+// unshift adds elements to the front of the array
+foodsILike.unshift('spinach');
+console.log(foodsILike);
+
+foodsILike.shift();
+console.log(foodsILike)
+
+// indexOf tells you the index of an element in the array; returns -1 if not found
+var index = foodsILike.indexOf('popcorn');
+console.log(index);
+
+} else {
+  // if it's not correct, tell them to try again
+  alert('Wrong! try again');
+}
+
+alert('you have ' + userPoints + ' points.');
+console.log('The user \'s total points are ' + userPoints);
