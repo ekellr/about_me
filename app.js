87567fa59bd1e915de74dd04d21839e46764685a
dmuldrew <dmuldrew@gmail.com>
2017-06-08 15:10:56 -0700

    added function to remove redundancy

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ var userPoints = 0;
 var myName = 'Elaine';
 var movieTimes = 100;
 var numberOfQuestions = 7;
+var answer;
+var answerArray = [];
 
+// definition of isRight function
+function isRight(answer, rightAnswer) {
+  if (answer === rightAnswer || answer === rightAnswer[0]) {
+    // if it's not correct, tell them to try again
+    userPoints++;
+  } else {
+    // if it's correct, give them a point
+    alert('Wrong! try again');
+  }
+  alert('you have ' + userPoints + ' points.');
+}
 
 alert('Welcome to my guessing game! My name is ' + myName + '!');
 console.log('Welcome message');
@@ -12,69 +25,96 @@ var user = prompt('What is your name?');
 console.log('The user\'s name is ' + user);
 
 
-var answer1 = prompt('Is ' + myName + '\'s favorite animal a bird?', 'type yes or no').toLowerCase();
-console.log('The user answer is ' + answer1);
+answer = prompt('Is ' + myName + '\'s favorite animal a bird?', 'type yes or no').toLowerCase();
+console.log('The user answer is ' + answer);
+
+// if (answer1 === 'yes' || answer1 === 'y') {
+//   // if it's correct, give them a point
+//   alert('Wrong! try again');
+// } else {
+//   // if it's not correct, tell them to try again
+//   userPoints++;
+// }
+
+answerArray.push(answer);
+
+var rightAnswer = 'no';
+isRight(answer, rightAnswer);
+
+// alert('you have ' + userPoints + ' points.');
+
+answer = prompt('Does ' + myName + ' have 7 chickens?').toLowerCase();
+console.log('The user answer is ' + answer);
+
+// if (answer2 === 'yes' || answer2 === 'y') {
+//   // if it's correct, give them a point
+//   userPoints++;
+// } else {
+//   // if it's not correct, tell them to try again
+//   alert('Wrong! try again');
+// }
+
+answerArray.push(answer);
+
+var rightAnswer = 'yes';
+isRight(answer, rightAnswer);
+
+// alert('you have ' + userPoints + ' points.');
+answer = prompt('Is Matrix ' + myName + '\'s favorite movie?', 'type yes or no').toLowerCase();
+console.log('The user answer is ' + answer);
+
+// if (answer3 === 'yes' || answer3 === 'y') {
+//   // if it's correct, give them a point
+//   alert('Wrong! try again');
+//
+// } else {
+//   // if it's not correct, tell them to try again
+//   userPoints++;
+// }
 
 
-if (answer1 === 'yes' || answer1 === 'y') {
-  // if it's correct, give them a point
-  alert('Wrong! try again');
-} else {
-  // if it's not correct, tell them to try again
-  userPoints++;
-}
+answerArray.push(answer);
 
-alert('you have ' + userPoints + ' points.');
+var rightAnswer = 'no';
+isRight(answer, rightAnswer);
 
-var answer2 = prompt('Does ' + myName + ' have 7 chickens?').toLowerCase();
-console.log('The user answer is ' + answer2);
+// alert('you have ' + userPoints + ' points.');
+answer = prompt('Is ' + myName + '\'s favorite food Paleo Pizza?', 'type yes or no').toLowerCase();
+console.log('The user answer is ' + answer);
 
-if (answer2 === 'yes' || answer2 === 'y') {
-  // if it's correct, give them a point
-  userPoints++;
-} else {
-  // if it's not correct, tell them to try again
-  alert('Wrong! try again');
-}
+// if (answer4 === 'yes' || answer4 === 'y') {
+//   // if it's correct, give them a point
+//   userPoints++;
+// } else {
+//   // if it's not correct, tell them to try again
+//   alert('Wrong! try again');
+// }
 
-alert('you have ' + userPoints + ' points.');
-var answer3 = prompt('Is Matrix ' + myName + '\'s favorite movie?', 'type yes or no').toLowerCase();
-console.log('The user answer is ' + answer3);
 
-if (answer3 === 'yes' || answer3 === 'y') {
-  // if it's correct, give them a point
-  alert('Wrong! try again');
+answerArray.push(answer);
 
-} else {
-  // if it's not correct, tell them to try again
-  userPoints++;
-}
+var rightAnswer = 'yes';
+isRight(answer, rightAnswer);
 
-alert('you have ' + userPoints + ' points.');
-var answer4 = prompt('Is ' + myName + '\'s favorite food Paleo Pizza?', 'type yes or no').toLowerCase();
-console.log('The user answer is ' + answer4);
+// alert('you have ' + userPoints + ' points.');
+answer = prompt('Is ' + myName + '\'s favorite car brand Audi?', 'type yes or no').toLowerCase();
+console.log('The user answer is ' + answer);
 
-if (answer4 === 'yes' || answer4 === 'y') {
-  // if it's correct, give them a point
-  userPoints++;
-} else {
-  // if it's not correct, tell them to try again
-  alert('Wrong! try again');
-}
+// if (answer5 === 'yes' || answer5 === 'y') {
+//   // if it's correct, give them a point
+//   userPoints++;
+// } else {
+//   // if it's not correct, tell them to try again
+//   alert('Wrong! Thanks for playing with me!!!!');
+// }
 
-alert('you have ' + userPoints + ' points.');
-var answer5 = prompt('Is ' + myName + '\'s favorite car brand Audi?', 'type yes or no').toLowerCase();
-console.log('The user answer is ' + answer5);;
 
-if (answer5 === 'yes' || answer5 === 'y') {
-  // if it's correct, give them a point
-  userPoints++;
-} else {
-  // if it's not correct, tell them to try again
-  alert('Wrong! Thanks for playing with me!!!!');
-}
+answerArray.push(answer);
 
-alert('You have ' + userPoints + ' points.');
+var rightAnswer = 'yes';
+isRight(answer, rightAnswer);
+
+// alert('You have ' + userPoints + ' points.');
 console.log('The user has ' + userPoints + ' points!!!');
 
 var answer6;
@@ -106,7 +146,7 @@ do{
 while(retryCount < 5);
 
 //question #7
-/*var foodsDislike = ['MSG', 'wheat', 'canola oil', 'soy', 'simple carbs'];
+var foodsDislike = ['MSG', 'wheat', 'canola oil', 'soy', 'simple carbs'];
 
 alert('Hi ' + user + '! Let\'s play another game:');
 var isCorrect = false;
@@ -115,12 +155,9 @@ var answer7 = prompt('Can you guess what food I try to avoid as much as I can?')
 console.log(answer7);
 
 for (var i = 0; i < foodsDislike.length; i++) {
-  if(answer7 == foodsDislike [i]){
+  if(answer7 == foodsDislike[i]){
     console.log('The user \'s guess was ' + foodsDislike);
     userPoints++;
-    do{
-      alert('You answer is' + foodsDislike[i]);
-    }
     isCorrect = true;
     break;
   }
@@ -130,4 +167,4 @@ if (isCorrect){
 }
 
 alert('you have ' + userPoints + ' points.');
-console.log('The user \'s total points are ' + userPoints);*/
+console.log('The user \'s total points are ' + userPoints);
